f487503d51b70f4cdea12310f4b87a8a4ffa202c
Sulav <60843792+sulavthapa197@users.noreply.github.com>
2022-02-17 22:52:06 +0545

    added extra function

--- tic.c
+++ tic.c
@@ -8,10 +8,13 @@ char board[3][3] = {'-','-','-','-','-','-','-','-','-'};//board for tictactoe g
 bool game_active = true;//Boolean to check the status of game
 
 char current_player = 'X';//current player(player to start first)
+
+char *winner = '\0'; // null type
 //variable declaration end
 
 //function declaration begin
 void display_board(char board[3][3]);
+void player_swap();
 //function declaration end
 
 
@@ -22,7 +25,10 @@ int main(){
 	return 0;
 }
 
-void display_board(char board[3][3]){ // function to display board 
+
+
+// function to display board 
+void display_board(char board[3][3]){ 
 	int i, j; // access the 2D array such that it displays the board correctly
 	for (i=0; i<3; i++ ){
 		for (j=0; j<3; j++){
@@ -31,5 +37,15 @@ void display_board(char board[3][3]){ // function to display board
 		}
 		printf("\n");	
 
+	}
+}
+
+// function to swap the players respectively
+void player_swap(){
+	if(current_player == 'X'){
+		current_player = 'O';
+	}
+	else if(current_player == 'O'){
+		current_player = 'X';
 	}
 }
