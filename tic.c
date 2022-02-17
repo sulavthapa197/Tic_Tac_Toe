b222c45e7e3eb102fb32c054d61cbe314930991b
Sulav <60843792+sulavthapa197@users.noreply.github.com>
2022-02-17 08:03:37 +0545

    Separate function to display board

--- tic.c
+++ tic.c
@@ -2,28 +2,34 @@
 #include <stdbool.h>
 
 // variable declaration start
-
 char board[3][3] = {'-','-','-','-','-','-','-','-','-'};//board for tictactoe game
 
 
 bool game_active = true;//Boolean to check the status of game
 
 char current_player = 'X';//current player(player to start first)
-
-
 //variable declaration end
 
-//main funtion
+//function declaration begin
+void display_board(char board[3][3]);
+//function declaration end
+
+
+
+//main funtion that begins the game
 int main(){
+	display_board(board);
+	return 0;
+}
+
+void display_board(char board[3][3]){ // function to display board 
 	int i, j; // access the 2D array such that it displays the board correctly
 	for (i=0; i<3; i++ ){
 		for (j=0; j<3; j++){
 			printf("\t");
 			printf("%c", board[i][j]);
 		}
-		printf("\n");
+		printf("\n");	
 
 	}
-
-	return 0;
 }
